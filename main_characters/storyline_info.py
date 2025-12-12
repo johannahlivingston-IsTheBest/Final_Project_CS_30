41aaa20f75c728f42b5d2610ed40d061ca25169d
Johannah Livingston <johannah.livingston@rbe.sk.ca>
2025-12-12 13:39:48 -0600

    Create storyline_info.py

---
+++ main_characters/storyline_info.py
@@ -0,0 +1,34 @@
+###############################################################################
+# Title: havent picked yet
+# Class: CS 30
+# Assignment: CS 30 Final
+# Name: Johanah, Atticus, Aditi
+# Version: 1.0
+# Date: dec 12th 2025
+###############################################################################
+'''This code is for all of the storyline, including dialouge, 
+descriptions, endings, beginings and interactions between 
+characters, objects, or just anything.'''
+###############################################################################
+# Imports and Global Variables------------------------------------------------- 
+# make a dictionary and class for the three main characters
+
+
+
+
+# make a dictionary and class for npc's 
+
+# make a dictionary and class for player
+
+# make a dictionary and class for the rooms/maps
+
+
+# Functions -------------------------------------------------------------------
+
+
+# Main ------------------------------------------------------------------------
+
+
+
+
+

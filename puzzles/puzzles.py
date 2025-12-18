a688e78e4fa6dd06750bd60efe0744e6ae948d97
Johannah Livingston <johannah.livingston@rbe.sk.ca>
2025-12-18 13:41:50 -0600

    Update puzzles.py

--- puzzles/puzzles.py
+++ puzzles/puzzles.py
@@ -15,11 +15,11 @@ unscramble scrambled word, and multiple choice/ termanology.
 # Imports and Global Variables------------------------------------------------- 
 import random 
 rid_list = [
-   """I'm sharp and shiny, held in a fight, A hero swings me with all their might. What am I?""", 
-    """ I have wings but I do not fly like a bird,I breathe fire and guard gold unheard. What am I?""",
-    """I wear a robe and carry a staff,I cast spells but never laugh. Who am I?"""
+"""I'm sharp and shiny, held in a fight, A hero swings me with all their might. What am I?""", 
+""" I have wings but I do not fly like a bird,I breathe fire and guard gold unheard. What am I?""",
+"""I wear a robe and carry a staff,I cast spells but never laugh. Who am I?"""
 ]
-# Classes -------------------------------------------------------------------
+# Classes ---------------------------------------------------------------------------------------------------------
 class Puzzles:
    def __init__(self):
       pass
@@ -28,22 +28,38 @@ class Puzzles:
    def match_des(self):
       pass
    def riddle(self,rid_list):
+     # Exam One
       num = 0
       for i in range(3):
          print("\n")
          print(str(i + 1)+ ". " + rid_list[i])
-      answer_one = input("\n What is the answer to the first riddle? ").lower
-      if answer_one == " sword":
-         print("Correct!")
-      else: 
-         print("wrong")
-         
+      answer_one = input("\n What is the answer to the first riddle? ").lower()
+      answer_two = input("\n What is the answer to the second riddle? ").lower()
+      answer_three = input("\n What is the answer to the third riddle? ").lower()
+      correct_answers_count = 0
+      if answer_one == "sword":
+         correct_answers_count += 1
+      if answer_two == "dragon":
+         correct_answers_count += 1
+      if answer_three == "wizard":
+         correct_answers_count += 1
+      print(correct_answers_count)
+      if correct_answers_count == 3:
+         print("\nYou got 100%")
+      elif correct_answers_count == 2:
+         print("\nYou got 67%" )    
+      elif correct_answers_count == 1:
+         print("\nYou got 33%" )   
+      elif correct_answers_count == 0:
+         print("\nYou failed" )     
+      print(f"\nYou got {correct_answers_count} out of 3 right ")
 
-        
+
+   
    def scramble(self):
       pass
    def multi_choice(self):
       pass
 puzzles = Puzzles()  
-# Main ------------------------------------------------------------------------
+# Main --------------------------------------------------------------------------------------------------------------
 puzzles.riddle(rid_list)

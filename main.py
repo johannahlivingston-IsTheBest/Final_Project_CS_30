00256f753360f71f5a588fc3c4bacaf4f528ea83
Johannah Livingston <johannah.livingston@rbe.sk.ca>
2026-01-14 13:19:26 -0600

    Changed and fixed act_seven

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from player_class import Player
 player = Player()
 
 # ACT 1
-#act_one.starting_text(player)
-#act_one.tutorial_intro(player)
+act_one.starting_text(player)
+act_one.tutorial_intro(player)
 
 # ACT 2
 #act_two.act_two_tutorial(player)
